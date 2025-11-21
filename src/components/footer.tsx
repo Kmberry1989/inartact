176a2b074c0175b309ab42e2db230174d3fe0cb2
Kmberry1989 <kylematthewberry@gmail.com>
2025-11-21 10:32:56 -0500

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <footer className="border-t py-6 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-          Built for the community. Documenting art and activism across Indiana.
+          This directory exists to serve as a public record and living document of the individual work and of the community achievements of Indiana artists, activists, and to chronicle their own revolutions.
         </p>
       </div>
     </footer>
